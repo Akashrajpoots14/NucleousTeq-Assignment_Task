e5bf72883942594efa64afb4f65e9024fbd2801a
Akash Rajpoot <akashrajpoots14@gmail.com>
2023-03-03 15:52:51 +0530

    Shape program completed

---
+++ TASK_3/Shape/Shape.java
@@ -0,0 +1,6 @@
+package TASK_3.Shape;
+
+public abstract class Shape {
+    void parameter(){};
+    void area(){};
+}
---
+++ TASK_3/Shape/Testshape.java
@@ -0,0 +1,12 @@
+package TASK_3.Shape;
+
+public class Testshape {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(10,30);
+        circle.area();
+        circle.parameter();
+        rectangle.area();
+        rectangle.parameter();
+    }
+}
